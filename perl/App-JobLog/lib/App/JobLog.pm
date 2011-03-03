f991f89ea65831547c9f5e338eaeb52d2c1287e2
David Houghton <dhoughton@complyoninc.com>
2011-03-03 14:18:05 -0500

    added minimal documentation

--- perl/App-JobLog/lib/App/JobLog.pm
+++ perl/App-JobLog/lib/App/JobLog.pm
@@ -1,7 +1,15 @@
 package App::JobLog;
 use App::Cmd::Setup -app;
 
-# ABSTRACT: turns baubles into trinkets
+# ABSTRACT: base of work log application
+
+=head1 DESCRIPTION
+
+C<App::JobLog> is a minimal extension of L<App::Cmd>. All it adds to a vanilla
+instance of this class is all unambiguous aliases of basic commands.
+
+=cut
+
 sub allow_any_unambiguous_abbrev { 1 }
 
 1;
