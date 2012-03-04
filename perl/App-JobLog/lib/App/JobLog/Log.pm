83847ce9ebc7f9cafa0e133a6656ccb253110671
David Houghton <dhoughton@complyoninc.com>
2012-03-04 12:50:46 -0500

    fixed scanning for notes from bottom of log

--- perl/App-JobLog/lib/App/JobLog/Log.pm
+++ perl/App-JobLog/lib/App/JobLog/Log.pm
@@ -517,7 +517,7 @@ sub find_notes {
             if ( $ll->is_event ) {
                 $c2 = DateTime->compare( $ll->time, $start );
                 last if $c2 < 0;
-                push @notes, App::JobLog::Log::Note->new($ll) if $ll->is_note;
+                unshift @notes, App::JobLog::Log::Note->new($ll) if $ll->is_note;
                 last unless $c2;
             }
         }
@@ -742,10 +742,12 @@ sub _scan_for_previous_note {
     for my $index ( $i .. $#$io ) {
         my $line = $io->[$index];
         my $ll   = App::JobLog::Log::Line->parse($line);
-        last if $ll->time > $e;
-        if ( $ll->is_note ) {
-            $previous       = App::JobLog::Log::Note->new($ll);
-            $previous_index = $index;
+        if ( $ll->is_event ) {
+            last if $ll->time > $e;
+            if ( $ll->is_note ) {
+                $previous       = App::JobLog::Log::Note->new($ll);
+                $previous_index = $index;
+            }
         }
     }
     return $previous_index // $i;
